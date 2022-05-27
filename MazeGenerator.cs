2de02bb433f01278aa798efb579d10fe6f09e499
mastersword792 <84593845+mastersword792@users.noreply.github.com>
2022-05-27 14:05:02 -0700

    Update MazeGenerator.cs
    I found the working script.

--- MazeGenerator.cs
+++ MazeGenerator.cs
@@ -20,85 +20,204 @@
  *                                                    *
 \* * * * * * * * * * * * * * * *  * * * * * * * * * * */
 
+// MazeGenerator.cs
+/*
+
+Sector Connection Directions
+Bit:  3  2    1    0
+Name: UP DOWN LEFT RIGHT
+
+Sector Data:
+Bit:  13-10 9-5  4
+Name: YLen  XLen IsRoom
+
+Sector Door Positions:
+Bit:  30-26 25-23 21-18 17-14
+Name: Up    Down  Left  Right
+
+
+*/
+
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace MazeGeneration
 {
-	public enum CellStatus { Unreached, Frontier, Painted, Final }
-	
-	public struct Coords
-	{
-		public int X;
-		public int Y;
-		public Coords(int X, int Y)
-		{
-			this.X = X;
-			this.Y = Y;
-		}
-	}
-	
-	public class Maze
+    public class MazeGeneration
     {
-        public int Height;
-        public int Width;
-        private Random Rand;
-
-        public Maze(int Width, int Height)
+        ///<summary>
+        ///Generates a maze using Prim's Algorithm.
+        ///</summary>
+        ///<param name="Width">How many cells wide the maze will be.</param>
+        ///<param name="Height">How many cells high the maze will be.</param>
+        ///<param name="Seed">The seed for generation. If null, the seed will be random.</param>
+        ///<return>Returns a visual representation of the generated maze. 'X' is a wall, ' ' is floor.</return>
+        public static char[,] Test(int Width, int Height, int? Seed)
         {
-            this.Height = Height;
-            this.Width = Width;
-            this.Rand = new Random();
-        }
-        
-        public int[,] Generate()
-        {
-            int[,] MapInt = new int[this.Height, this.Width];
-            CellStatus[,] PaintedCells = new CellStatus[this.Height, this.Width];
-            Coords Start = new Coords(this.Rand.Next(this.Width), this.Rand.Next(this.Height));
-
-            for (int i = 0; i < this.Height; i++)
+            uint[,] maze = Generarion.Generate(Width, Height, Seed);
+            char[,] display = new char[3 * Height, 3 * Width];
+            for (int y = 0; y < maze.GetLength(0); y++)
             {
-                for (int j = 0; j < this.Width; j++)
+                for(int x = 0; x < maze.GetLength(1); x++)
                 {
+                    int X = (3 * x) + 1;
+                    int Y = (3 * y) + 1;
+                    display[Y - 1, X - 1] = 'X';
+                    display[Y - 1, X + 1] = 'X';
+                    display[Y + 1, X - 1] = 'X';
+                    display[Y + 1, X + 1] = 'X';
+                    display[Y, X] = ' ';
+                    display[Y - 1, X] = ((maze[y, x] & (uint)Direction.UP) == (uint)Direction.UP) ? ' ' : 'X';
+                    display[Y + 1, X] = ((maze[y, x] & (uint)Direction.DOWN) == (uint)Direction.DOWN) ? ' ' : 'X';
+                    display[Y, X - 1] = ((maze[y, x] & (uint)Direction.LEFT) == (uint)Direction.LEFT) ? ' ' : 'X';
+                    display[Y, X + 1] = ((maze[y, x] & (uint)Direction.RIGHT) == (uint)Direction.RIGHT) ? ' ' : 'X';
+                }
+            }
+            return display;
+        }
+    }
+
+    public enum Direction
+    {
+        NULL = 0, UP = 8, DOWN = 4, LEFT = 2, RIGHT = 1
+    }
+
+    internal enum CellStatus
+    {
+        Painted, Frontier, Unreached, NULL
+    }
+
+    public class Coords
+    {
+        public int X;
+        public int Y;
+
+        public Coords(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public static Direction GetDirection(Coords c1, Coords c2)
+        {
+            int dy = c2.Y - c1.Y;
+            int dx = c2.X - c1.X;
+
+            if (dy < 0) return Direction.UP;
+            else if (dy > 0) return Direction.DOWN;
+            else if (dx < 0) return Direction.LEFT;
+            else if (dx > 0) return Direction.RIGHT;
+            else return Direction.NULL;
+        }
+
+        public static Direction GetOppositeDirection(Coords c1, Coords c2)
+        {
+            int dy = c2.Y - c1.Y;
+            int dx = c2.X - c1.X;
+
+            if (dy < 0) return Direction.DOWN;
+            else if (dy > 0) return Direction.UP;
+            else if (dx < 0) return Direction.RIGHT;
+            else if (dx > 0) return Direction.LEFT;
+            else return Direction.NULL;
+        }
+    }
+
+    public class Generation
+    {
+        private List<Coords> GetSurroundingCells(Coords selected, int Width, int Height)
+        {
+            int x = selected.X;
+            int y = selected.Y;
+            List<Coords> n = new List<Coords>();
+
+            if (x > 0)
+            {
+                n.Add(new Coords(x - 1, y));
+            }
+
+            if (x < (Width - 1))
+            {
+                n.Add(new Coords(x + 1, y));
+            }
+
+            if (y > 0)
+            {
+                n.Add(new Coords(x, y - 1));
+            }
+
+            if (y < (Height - 1))
+            {
+                n.Add(new Coords(x, y + 1));
+            }
+
+            return n;
+        }
+
+        #region Prim's Algorithm --> Complete.
+        /// <summary>
+        /// Generates a maze based around Prim's Algorithm.
+        /// </summary>
+        /// <param name="Seed"></param>
+        /// <returns>Returns an encoded 2D unsigned integer array that describes the connections between rooms.
+        /// bin(0) = no door, bin(1) = door. <br/>
+        /// Position: 3      2       1       0 <br/>
+        /// Diection: UP     DOWN    RIGHT   LEFT
+        /// </returns>
+        /// 
+        public static uint[,] Generate(int Width, int Height, int? Seed)
+        {
+            Random Rand = (Seed == null)? new Random() : new Random((int)Seed);
+            uint[,] MapInt = new uint[Height, Width];
+            CellStatus[,] PaintedCells = new CellStatus[Height, Width];
+            Coords Start = new Coords(Rand.Next(Width), Rand.Next(Height));
+
+            /// TODO: Ensure MapInt returns the MetaData in reference to the spreadsheet "%userprofile%\Desktop\MapInt Metadata.xlsx"
+            /// 
+            //byte coordsMeta = 0x00;
+            for (byte i = 0; i < Height; i++)
+            {
+                for (byte j = 0; j < Width; j++)
+                {
+                    /*
+                    coordsMeta |= (byte)(i << 16);
+                    coordsMeta |= (byte)(i << 24);
+                    */
+
                     MapInt[i, j] = 0;
                     PaintedCells[i, j] = CellStatus.Unreached;
                 }
             }
 
-            return PrimsAlgorithm(MapInt, PaintedCells, Start);
+            return Algorithm(MapInt, PaintedCells, Start, Width, Height, Rand);
         }
 
-        #region int[,] Generate.PrimsAlgorithm() --> Generates a Maze using Prim's Algorithm.
-        private int[,] PrimsAlgorithm(int[,] MapInt, CellStatus[,] PaintedCells, Coords Start)
+        private static uint[,] Algorithm(uint[,] MapInt, CellStatus[,] PaintedCells, Coords Start, int Width, int Height, Random Rand)
         {
-            List<Coords> c1 = new List<Coords>();
-            List<Coords> c2 = new List<Coords>();
+            List<Coords> adj = new List<Coords>();
+            List<Coords> frontier = new List<Coords>();
             do
             {
-                c1.Clear();
-                c2.Clear();
+                adj.Clear();
+                frontier.Clear();
 
                 int x = Start.X;
                 int y = Start.Y;
 
                 PaintedCells[y, x] = CellStatus.Painted;
 
-                c1 = this.GetAdjacentPaintedCells(Start, PaintedCells);
+                adj = GetAdjacentPaintedCells(Start, PaintedCells, Width, Height);
 
-                if (c1.Count > 0)
+                if (adj.Count > 0)
                 {
-                    Coords n;
-                    if (c1.Count == 1)
-                    {
-                        n = c1[0];
-                    }
-                    else
-                    {
-                        n = c1[this.Rand.Next(c1.Count)];
-                    }
-
-                    Direction d = GetDirection(Start, n);
+                    Coords chosen = (adj.Count == 1) ? adj[0] : adj[Rand.Next(adj.Count)];
+                    //Direction d = Coords.GetDirection(Start, n);
+                    
+                    MapInt[y, x] |= (uint)Coords.GetDirection(Start, chosen);
+                    MapInt[chosen.Y, chosen.X] |= (uint)Coords.GetOppositeDirection(Start, chosen);
+                    
+                    /*
                     if (d == Direction.UP)
                     {
                         MapInt[y, x] |= (1 << 3);
@@ -119,44 +238,44 @@ namespace MazeGeneration
                         MapInt[y, x] |= (1 << 0);
                         MapInt[n.Y, n.X] |= (1 << 1);
                     }
+                    */
                 }
 
-                PaintedCells = this.PaintAdjacentCellsFrontier(Start, PaintedCells);
+                PaintedCells = PaintAdjacentCellsFrontier(Start, PaintedCells, Width, Height);
 
-                c2 = this.GetFrontierCells(PaintedCells);
-                
-                if (c2.Count == 1) Start = c2[0];
-                else if(c2.Count > 1) Start = c2[this.Rand.Next(c2.Count)];
-            } while (c2.Count > 0);
+                frontier = GetFrontierCells(PaintedCells, Width, Height);
 
+                if (frontier.Count == 1) Start = frontier[0];
+                else if (frontier.Count > 1) Start = frontier[Rand.Next(frontier.Count)];
+            } while (frontier.Count > 0);
             return MapInt;
         }
 
-        private List<Coords> GetAdjacentPaintedCells(Coords c, CellStatus[,] PaintedCells)
+        private static List<Coords> GetAdjacentPaintedCells(Coords c, CellStatus[,] PaintedCells, int Width, int Height)
         {
             int x = c.X;
             int y = c.Y;
             List<Coords> n = new List<Coords>();
 
-            if(x > 0)
+            if (x > 0)
             {
-                if(PaintedCells[y, x - 1] == CellStatus.Painted)
+                if (PaintedCells[y, x - 1] == CellStatus.Painted)
                     n.Add(new Coords(x - 1, y));
             }
 
-            if(x < (this.Width - 1))
+            if (x < (Width - 1))
             {
                 if (PaintedCells[y, x + 1] == CellStatus.Painted)
                     n.Add(new Coords(x + 1, y));
             }
 
-            if(y > 0)
+            if (y > 0)
             {
                 if (PaintedCells[y - 1, x] == CellStatus.Painted)
                     n.Add(new Coords(x, y - 1));
             }
 
-            if(y < (this.Height - 1))
+            if (y < (Height - 1))
             {
                 if (PaintedCells[y + 1, x] == CellStatus.Painted)
                     n.Add(new Coords(x, y + 1));
@@ -165,7 +284,7 @@ namespace MazeGeneration
             return n;
         }
 
-        private CellStatus[,] PaintAdjacentCellsFrontier(Coords c, CellStatus[,] PaintedCells)
+        private static CellStatus[,] PaintAdjacentCellsFrontier(Coords c, CellStatus[,] PaintedCells, int Width, int Height)
         {
             int x = c.X;
             int y = c.Y;
@@ -176,7 +295,7 @@ namespace MazeGeneration
                     PaintedCells[y, x - 1] = CellStatus.Frontier;
             }
 
-            if (x < (this.Width - 1))
+            if (x < (Width - 1))
             {
                 if (PaintedCells[y, x + 1] == CellStatus.Unreached)
                     PaintedCells[y, x + 1] = CellStatus.Frontier;
@@ -188,7 +307,7 @@ namespace MazeGeneration
                     PaintedCells[y - 1, x] = CellStatus.Frontier;
             }
 
-            if (y < (this.Height - 1))
+            if (y < (Height - 1))
             {
                 if (PaintedCells[y + 1, x] == CellStatus.Unreached)
                     PaintedCells[y + 1, x] = CellStatus.Frontier;
@@ -197,12 +316,12 @@ namespace MazeGeneration
             return PaintedCells;
         }
 
-        private List<Coords> GetFrontierCells(CellStatus[,] PaintedCells)
+        private static List<Coords> GetFrontierCells(CellStatus[,] PaintedCells, int Width, int Height)
         {
             List<Coords> n = new List<Coords>();
-            for (int y = 0; y < this.Height; y++)
+            for (int y = 0; y < Height; y++)
             {
-                for (int x = 0; x < this.Width; x++)
+                for (int x = 0; x < Width; x++)
                 {
                     if (PaintedCells[y, x] == CellStatus.Frontier)
                     {
@@ -210,22 +329,42 @@ namespace MazeGeneration
                     }
                 }
             }
-            
+
             return n;
-        }
-
-        private static Direction GetDirection(Coords c, Coords n)
-        {
-            int dx = n.X - c.X;
-            int dy = n.Y - c.Y;
-
-            if (dx == 1) return Direction.RIGHT;
-            if (dx == -1) return Direction.LEFT;
-            if (dy == 1) return Direction.DOWN;
-            if (dy == -1) return Direction.UP;
-
-            return Direction.NULL;
         }
         #endregion
     }
+
+    public class PostGeneration
+    {
+        ///<summary> Generates multiple inaccessible holes in the map.</summary>
+        ///<param Name='MapInt'>The generated map data.</param>
+        ///<param Name='HoleLocs'>The list of locations within the map to generate hole.</param>
+        ///<return>Returns the generated map data with the holes in it.</return>
+        public static uint[,] GenerateMultipleHoles(uint[,] MapInt, List<Coords> HoleLocs)
+        {
+            var output = MapInt;
+            foreach(Coords c in HoleLocs)
+            {
+                output = GenerateHole(output, c);
+            }
+            return output;
+        }
+
+        ///<summary> Generates an inaccessible hole in the map.</summary>
+        ///<param Name='MapInt'>The generated map data.</param>
+        ///<param Name='HoleLoc'>The location within the map to generate a hole.</param>
+        ///<return>Returns the generated map data with the holes in it.</return>
+        public static uint[,] GenerateHole(uint[,] MapInt, Coords HoleLoc)
+        {
+            uint[,] output = MapInt;
+            output[HoleLoc.Y, HoleLoc.X] = 0;
+            output[HoleLoc.Y, HoleLoc.X + 1] &= ~(1);
+            output[HoleLoc.Y, HoleLoc.X - 1] &= ~(1 << 1);
+            output[HoleLoc.Y - 1, HoleLoc.X] &= ~(1 << 2);
+            output[HoleLoc.Y + 1, HoleLoc.X] &= ~(1 << 3);
+            return output;
+        }
+    }
+
 }
